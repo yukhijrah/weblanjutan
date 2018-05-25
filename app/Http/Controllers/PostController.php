c1acb39190ebfbcc0ef05c71683288dac58275ce
mAlwiMhfd <muhammadalwi27@gmail.com>
2018-05-25 16:40:05 +0700

    tambah method alert

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use Session;
 
 class PostController extends Controller
 {
@@ -59,6 +60,7 @@ class PostController extends Controller
         $post->save();
 
         if($post->save()) {
+            Session::flash('success','Post berhasil ditambah');
             return redirect()->route('posts.index');
         }
 
@@ -121,6 +123,7 @@ class PostController extends Controller
         $post=Post::find($id);
         $post->delete();
 
+        Session::flash('success','Post berhasil didelete');
         return redirect()->route('posts.index');
     }
 }
